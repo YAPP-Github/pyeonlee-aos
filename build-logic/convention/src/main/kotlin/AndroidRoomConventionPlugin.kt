f312e0c172614829ab7bbcf265ef1acd4647754a
aurora32s <aurora32s@naver.com>
2023-05-18 14:40:10 +0900

    [BASE] #2 - core module 들의 build.gradle 코드 정리

--- build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
@@ -1,8 +1,15 @@
+import com.google.devtools.ksp.gradle.KspExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.process.CommandLineArgumentProvider
+import java.io.File
 
 class AndroidRoomConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -11,11 +18,23 @@ class AndroidRoomConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.google.devtools.ksp")
             }
+            extensions.configure<KspExtension> {// This is required to enable Room auto migrations..
+                arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
+            }
+
             dependencies {
                 "implementation"(libs.findLibrary("androidx.room.runtime").get())
                 "implementation"(libs.findLibrary("androidx.room.ktx").get())
                 "ksp"(libs.findLibrary("androidx.room.compiler").get())
             }
         }
+    }
+
+    class RoomSchemaArgProvider(
+        @get:InputDirectory
+        @get:PathSensitive(PathSensitivity.RELATIVE)
+        val schemaDir: File,
+    ) : CommandLineArgumentProvider {
+        override fun asArguments() = listOf("room.schemaLocation=${schemaDir.path}")
     }
 }
--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-@Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     id("peonlee.android.library")
     id("peonlee.android.hilt")
@@ -22,17 +5,9 @@ plugins {
 
 android {
     namespace = "com.peonlee.core.data"
-
-    defaultConfig {
-        consumerProguardFiles("consumer-rules.pro")
-    }
 }
 
 dependencies {
     implementation(project(":core-database"))
     implementation(libs.kotlinx.coroutines.android)
-
-    // Local tests: jUnit, coroutines, Android runner
-    testImplementation(libs.junit)
-    testImplementation(libs.kotlinx.coroutines.test)
 }
