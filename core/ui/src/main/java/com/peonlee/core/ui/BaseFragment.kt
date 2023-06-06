66f617042e9ab185d43c23c3f3ffe2ef96a94a11
jiwon2724 <stopone3119@naver.com>
2023-06-07 01:57:02 +0900

    [REFACTOR]#5 - 미사용 resource 수정 및 제거

--- core/ui/src/main/java/com/peonlee/core/ui/BaseFragment.kt
+++ core/ui/src/main/java/com/peonlee/core/ui/BaseFragment.kt
@@ -8,15 +8,5 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class BaseFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_base, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 }
