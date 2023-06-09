9c89196c0afe5874020c469d1780d8da7e007d25
jiwon2724 <stopone3119@naver.com>
2023-06-09 17:53:37 +0900

    [EDIT]#12 - core-data Hilt Module 추가

--- build-logic/convention/src/main/kotlin/ext/configureData.kt
+++ build-logic/convention/src/main/kotlin/ext/configureData.kt
@@ -3,11 +3,16 @@ package ext
 import com.android.build.api.dsl.CommonExtension
 import const.Keys
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 internal fun Project.configureData(
     commonExtension: CommonExtension<*, *, *, *>
 ) {
     commonExtension.apply {
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
         defaultConfig {
             buildConfigField(
                 type = "String",
@@ -21,6 +26,14 @@ internal fun Project.configureData(
 
         buildFeatures {
             buildConfig = true
+        }
+
+        dependencies {
+            "implementation"(libs.findLibrary("squareup.okhttp3").get())
+            "implementation"(libs.findLibrary("squareup.okhttp3.logging.interceptor").get())
+            "implementation"(libs.findLibrary("squareup.retrofit2").get())
+            "implementation"(libs.findLibrary("serialization.converter").get())
+            "implementation"(libs.findLibrary("serialization.json").get())
         }
     }
 }
---
+++ core/data/src/main/java/com/peonlee/data/di/common/ConnectionInfo.kt
@@ -0,0 +1,7 @@
+package com.peonlee.data.di.common
+
+object ConnectionInfo {
+    const val APPLICATION_JSON = "application/json"
+
+    const val TIME_OUT = 60L
+}
