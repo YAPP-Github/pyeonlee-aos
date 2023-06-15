354fbdd126f919ac798b1d7b83f5087aa5c5587e
aurora32s <aurora32s@naver.com>
2023-06-15 13:02:45 +0900

    [BASE] #9 - BaseFragment에 boilerplate 코드 정리

--- core/ui/src/main/java/com/peonlee/core/ui/base/BaseFragment.kt
+++ core/ui/src/main/java/com/peonlee/core/ui/base/BaseFragment.kt
@@ -1,6 +1,30 @@
-
 package com.peonlee.core.ui.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 
-class BaseFragment : Fragment()
+abstract class BaseFragment<T : ViewBinding> : Fragment() {
+    private lateinit var _binding: T
+    val binding: T get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = container?.let { bindingFactory(it) } ?: return null
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    abstract fun bindingFactory(parent: ViewGroup): T
+    open fun initViews() {}
+}
