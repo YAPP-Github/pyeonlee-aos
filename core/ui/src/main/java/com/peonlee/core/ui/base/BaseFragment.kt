485ca8487eb6937bd13bffc338f1095488cb9886
aurora32s <aurora32s@naver.com>
2023-06-16 13:28:09 +0900

    [REFACTOR] #9 - BaseFragment onDestroy 시 _binding null로 초기화

--- core/ui/src/main/java/com/peonlee/core/ui/base/BaseFragment.kt
+++ core/ui/src/main/java/com/peonlee/core/ui/base/BaseFragment.kt
@@ -8,16 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<T : ViewBinding> : Fragment() {
-    private lateinit var _binding: T
-    val binding: T get() = _binding
+    private var _binding: T? = null
+    val binding: T get() = _binding ?: error("binding not initialized yet")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = container?.let { bindingFactory(it) } ?: return null
-        return _binding.root
+        _binding = container?.let { bindingFactory(it) }
+        return _binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,4 +27,9 @@ abstract class BaseFragment<T : ViewBinding> : Fragment() {
 
     abstract fun bindingFactory(parent: ViewGroup): T
     open fun initViews() {}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
