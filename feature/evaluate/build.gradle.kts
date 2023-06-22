bae03ceb0994351c26ef6ce87b22a8dfdce3e468
jiwon2724 <stopone3119@naver.com>
2023-06-23 00:40:55 +0900

    [BASE]#15 - evaluate 모듈 생성

---
+++ feature/evaluate/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("peonlee.android.feature")
+}
+
+android {
+    namespace = "com.peonlee.evaluate"
+    viewBinding { enable = true }
+}
+
+dependencies {
+}
---
+++ feature/evaluate/src/main/java/com/peonlee/evaluate/OnboardActivity.kt
@@ -0,0 +1,11 @@
+package com.peonlee.evaluate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboading)
+    }
+}
