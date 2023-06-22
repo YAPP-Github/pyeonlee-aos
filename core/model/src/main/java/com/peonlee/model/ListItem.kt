3d713fb7ebcc18823575b72c5a2f42896f490cbe
aurora32s <aurora32s@naver.com>
2023-06-22 23:09:39 +0900

    [FEAT] #9 - 최근리뷰 UiModel(RecentReviewUiModel) 추가

--- core/model/src/main/java/com/peonlee/model/ListItem.kt
+++ core/model/src/main/java/com/peonlee/model/ListItem.kt
@@ -12,5 +12,6 @@ interface MainHomeListItem : ListItem
 enum class MainHomeViewType {
     TITLE, // 제목
     DIVIDER, // 구분선
-    CONDITIONAL_PRODUCTS // 조건별 상품 리스트
+    CONDITIONAL_PRODUCTS, // 조건별 상품 리스트
+    RECENT_REVIEW // 최신 리뷰
 }
