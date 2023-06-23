a29624d606e3bd02af456884f239fcb6ad205013
jiwon2724 <stopone3119@naver.com>
2023-06-23 17:38:51 +0900

    [BASE]#15 - BaseActivity, Fragment 적용

--- feature/evaluate/src/main/java/com/peonlee/evaluate/EvaluateFragment.kt
+++ feature/evaluate/src/main/java/com/peonlee/evaluate/EvaluateFragment.kt
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class EvaluateFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+import com.peonlee.core.ui.base.BaseFragment
+import com.peonlee.evaluate.databinding.FragmentEvaluateBinding
 
+class EvaluateFragment : BaseFragment<FragmentEvaluateBinding>() {
+    override fun bindingFactory(parent: ViewGroup): FragmentEvaluateBinding {
+        return FragmentEvaluateBinding.inflate(layoutInflater, parent, false)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_evaluate, container, false)
+    override fun initViews() {
+
     }
 }
--- feature/evaluate/src/main/java/com/peonlee/evaluate/OnboardActivity.kt
+++ feature/evaluate/src/main/java/com/peonlee/evaluate/OnboardActivity.kt
@@ -2,10 +2,19 @@ package com.peonlee.evaluate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.peonlee.core.ui.base.BaseActivity
+import com.peonlee.evaluate.databinding.ActivityOnboadingBinding
 
-class OnboardActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboading)
+class OnboardActivity : BaseActivity<ActivityOnboadingBinding>() {
+    override fun bindingFactory(): ActivityOnboadingBinding {
+        return ActivityOnboadingBinding.inflate(layoutInflater)
+    }
+
+    override fun initViews() {
+
+    }
+
+    override fun bindViews() {
+
     }
 }
