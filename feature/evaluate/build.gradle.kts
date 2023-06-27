1b0cf85b8a652b206c0d9f90cc0263c9edec4c49
정지원 <stopone3119@gmail.com>
2023-06-27 14:33:50 +0900

    [FEATRUE] #15 온보딩, 평가화면 모듈 생성 및 UI 구현 (#31)
    * [BASE]#15 - evaluate 모듈 생성
    
    * [UI]#15 - 평가화면 UI 구현
    
    * [BASE]#15 - BaseActivity, Fragment 적용
    
    * [REFACTOR]#15 - XML ID 매핑
    
    * [REFACTOR]#15 - ktlint 수정사항 반영
    
    * [REFACTOR]#15 - ktlint 수정사항 반영
    
    * [REFACTOR]#15 - ktlint 수정사항 반영
    
    * [REFACTOR]#15 - ktlint 수정사항 반영
    
    * [REFACTOR]#15 - manifest tag값 설정
    
    * [REFACTOR]#15 - PR 코멘트 반영 완료

---
+++ feature/evaluate/build.gradle.kts
@@ -0,0 +1,17 @@
+
+plugins {
+    id("peonlee.android.feature")
+}
+
+android {
+    namespace = "com.peonlee.evaluate"
+    viewBinding { enable = true }
+}
+
+dependencies {
+    implementation(project(":core:ui"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:data"))
+
+    implementation(libs.androidx.recyclerview)
+}
---
+++ feature/evaluate/src/main/java/com/peonlee/evaluate/EvaluateFragment.kt
@@ -0,0 +1,14 @@
+package com.peonlee.evaluate
+
+import android.view.ViewGroup
+import com.peonlee.core.ui.base.BaseFragment
+import com.peonlee.evaluate.databinding.FragmentEvaluateBinding
+
+class EvaluateFragment : BaseFragment<FragmentEvaluateBinding>() {
+    override fun bindingFactory(parent: ViewGroup): FragmentEvaluateBinding {
+        return FragmentEvaluateBinding.inflate(layoutInflater, parent, false)
+    }
+
+    override fun initViews() {
+    }
+}
