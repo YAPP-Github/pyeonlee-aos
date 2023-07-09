8a147416be2f57eb2e6af57836929e7f9e3071d7
aurora32s <aurora32s@naver.com>
2023-07-09 16:49:17 +0900

    [FEAT] #45 - 리뷰 저장 요청 API 추가

--- core/data/src/main/java/com/peonlee/data/review/ReviewRepository.kt
+++ core/data/src/main/java/com/peonlee/data/review/ReviewRepository.kt
@@ -1,5 +1,8 @@
 package com.peonlee.data.review
 
+import com.peonlee.data.Result
+import com.peonlee.data.model.review.SaveReviewResponse
+
 /**
  * 리뷰와 관련된 Repository
  */
@@ -8,6 +11,7 @@ interface ReviewRepository {
      * 새로운 리뷰 저장
      * @param productId 리뷰를 작성 하는 상품의 id
      * @param review 사용자가 작성한 리뷰
+     * @return 리뷰 저장 API Response
      */
-    suspend fun saveReview(productId: Int, review: String): Result<Unit>
+    suspend fun saveReview(productId: Int, review: String): Result<SaveReviewResponse>
 }
