6be9e8aaae3dc6278549d84e595c1ee69d61ba3c
aurora32s <aurora32s@naver.com>
2023-07-09 18:31:13 +0900

    [FEAT] #39 - 마이페이지 view binding 추가

--- feature/user/src/main/java/com/peonlee/user/UserFragment.kt
+++ feature/user/src/main/java/com/peonlee/user/UserFragment.kt
@@ -17,6 +17,15 @@ class UserFragment : BaseFragment<FragmentUserBinding>() {
         )
     }
 
+    override fun initViews() = with(binding) {
+        // TODO 실제 사용자 정보로 대체 필요
+        tvNickname.text = "꿈꾸는날다람쥐"
+        // 작성한 리뷰 개수
+        tvCommentCount.text = getString(R.string.count, 24)
+        // 평가한 상품 개수
+        tvEvaluateCount.text = getString(R.string.count, 102)
+    }
+
     companion object {
         fun getInstance() = UserFragment()
     }
