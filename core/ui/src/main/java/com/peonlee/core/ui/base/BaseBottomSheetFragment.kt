3fb758f509f3c08b829b260bed87f294d605f897
aurora32s <aurora32s@naver.com>
2023-07-11 21:48:05 +0900

    [FEAT] #38 - BottomSheet 기본 layout 구성

--- core/ui/src/main/java/com/peonlee/core/ui/base/BaseBottomSheetFragment.kt
+++ core/ui/src/main/java/com/peonlee/core/ui/base/BaseBottomSheetFragment.kt
@@ -5,21 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.peonlee.core.ui.databinding.BaseBottomsheetDialogBinding
 
 /**
  * BottomSheetFragment 공통 코드 관리
  */
 class BaseBottomSheetFragment : BottomSheetDialogFragment() {
 
+    private lateinit var binding: BaseBottomsheetDialogBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = BaseBottomsheetDialogBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvTitle.text = "Hello WOrld"
     }
 }
