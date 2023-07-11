50480aae0ff768089b640ea37a9f6e28f2f44982
aurora32s <aurora32s@naver.com>
2023-07-11 22:48:27 +0900

    [FEAT] #38 - 가격 필터링 아이템 추가

--- feature/explore/src/main/java/com/peonlee/explore/ui/PriceFilterBottomSheetFragment.kt
+++ feature/explore/src/main/java/com/peonlee/explore/ui/PriceFilterBottomSheetFragment.kt
@@ -2,11 +2,29 @@ package com.peonlee.explore.ui
 
 import android.view.View
 import com.peonlee.core.ui.base.BaseBottomSheetFragment
+import com.peonlee.explore.databinding.ItemPriceFilterBinding
 import com.peonlee.explore.databinding.LayoutPriceFilterBinding
 
 class PriceFilterBottomSheetFragment : BaseBottomSheetFragment("가격") {
     override fun getFilterLayout(): View {
-        val radioGroup = LayoutPriceFilterBinding.inflate(layoutInflater)
-        return radioGroup.root
+        val radioGroup = LayoutPriceFilterBinding.inflate(layoutInflater).root
+
+        // 하위 Filter 추가
+        PriceFilter.values().forEach {
+            val priceFilterItem = ItemPriceFilterBinding.inflate(layoutInflater).root
+            priceFilterItem.text = it.minPrice.toString()
+            radioGroup.addView(priceFilterItem)
+        }
+        return radioGroup
+    }
+
+    private enum class PriceFilter(
+        val minPrice: Int,
+        val maxPrice: Int?
+    ) {
+        ONE_POINT_FIVE(0, 1_500),
+        FIVE_ZERO(1_500, 5_000),
+        TEN_ZERO(5_000, 10_000),
+        OVER_TEN(10_000, null)
     }
 }
