df318da9bef60fb8a1a13359f59f385da91fd558
aurora32s <aurora32s@naver.com>
2023-07-11 21:38:08 +0900

    [FEAT] #38 - BottomSheetDialogFragment Base 클래스 생성

---
+++ core/ui/src/main/java/com/peonlee/core/ui/base/BaseBottomSheetFragment.kt
@@ -0,0 +1,25 @@
+package com.peonlee.core.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * BottomSheetFragment 공통 코드 관리
+ */
+class BaseBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
