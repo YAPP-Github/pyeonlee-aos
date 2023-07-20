52f7cfbc1b0d9fc2fe3b2f2ad85d04b8c5747a18
jiwon2724 <stopone3119@naver.com>
2023-07-20 20:24:25 +0900

    [FEATURE] #55 SplashActivity 자동 로그인 구현

--- app/src/main/java/com/peonlee/ui/SplashActivity.kt
+++ app/src/main/java/com/peonlee/ui/SplashActivity.kt
@@ -3,27 +3,45 @@ package com.peonlee.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.ViewTreeObserver
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.peonlee.R
 import com.peonlee.login.LoginActivity
+import com.peonlee.login.LoginViewModel
+import com.peonlee.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    private val loginViewModel: LoginViewModel by viewModels()
+    private var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_splash)
+        delayScreen()
+    }
+
+    private fun delayScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({ isReady = true }, DELAY)
 
         val content: View = findViewById(android.R.id.content)
         content.viewTreeObserver.addOnPreDrawListener(
             object : ViewTreeObserver.OnPreDrawListener {
                 override fun onPreDraw(): Boolean {
-                    return if (true) {
+                    return if (isReady) {
                         content.viewTreeObserver.removeOnPreDrawListener(this)
-                        startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                        finish()
+                        observe()
                         true
                     } else {
                         false
@@ -31,5 +49,24 @@ class SplashActivity : AppCompatActivity() {
                 }
             }
         )
+    }
+
+    private fun observe() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.getToken().collect { accessToken ->
+                    if (accessToken.isNotEmpty()) {
+                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    } else {
+                        startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                    }
+                    finish()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val DELAY = 1500L
     }
 }
