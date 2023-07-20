94dbedd5a8bc1d7f80ad7d8c05897c7c2bb47662
seomseom <jyj@paytalab.com>
2023-07-20 12:04:03 +0900

    [FEAT] Auth Interceptor 추가

---
+++ core/data/src/main/java/com/peonlee/data/interceptor/AuthInterceptor.kt
@@ -0,0 +1,17 @@
+package com.peonlee.data.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val authRequest = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjg5NzcwNDk1LCJleHAiOjE3MjEzOTI4OTV9.xKIWAW5GWjmETFHwTppWK2bvXKWHAsgwxBpKKsXbxmA")
+            .build()
+
+        println(authRequest)
+        return chain.proceed(authRequest)
+    }
+}
