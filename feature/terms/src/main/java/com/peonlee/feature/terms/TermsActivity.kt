67fa1b013afe278623366a635b0831e3b432118d
jiwon2724 <stopone3119@naver.com>
2023-07-21 17:07:02 +0900

    [UI] #75 약관동의 UI 구현

--- feature/terms/src/main/java/com/peonlee/feature/terms/TermsActivity.kt
+++ feature/terms/src/main/java/com/peonlee/feature/terms/TermsActivity.kt
@@ -2,6 +2,7 @@ package com.peonlee.feature.terms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.peonlee.terms.R
 
 class TermsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
