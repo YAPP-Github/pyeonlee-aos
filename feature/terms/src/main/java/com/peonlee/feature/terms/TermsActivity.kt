ec8beae40c8dd70ba627f0298e1ca02525ab37db
jiwon2724 <stopone3119@naver.com>
2023-07-21 13:30:42 +0900

    [BASE] #75 약관동의 모듈 생성

---
+++ feature/terms/src/main/java/com/peonlee/feature/terms/TermsActivity.kt
@@ -0,0 +1,11 @@
+package com.peonlee.feature.terms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms)
+    }
+}
