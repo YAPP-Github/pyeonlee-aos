462ed0aac1dfd472ebbb0cd3a4f4e695b9abbc28
jiwon2724 <stopone3119@naver.com>
2023-07-21 19:31:07 +0900

    [FEATURE] #75 UI 로직 구현

--- feature/terms/src/main/java/com/peonlee/feature/terms/TermsActivity.kt
+++ feature/terms/src/main/java/com/peonlee/feature/terms/TermsActivity.kt
@@ -1,12 +1,64 @@
 package com.peonlee.feature.terms
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.peonlee.terms.R
+import android.content.res.ColorStateList
+import androidx.core.content.ContextCompat
+import com.peonlee.core.ui.base.BaseActivity
+import com.peonlee.core.ui.R.color as Color
+import com.peonlee.core.ui.R.drawable as Drawable
+import com.peonlee.terms.databinding.ActivityTermsBinding
 
-class TermsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms)
+class TermsActivity : BaseActivity<ActivityTermsBinding>() {
+    override fun bindingFactory(): ActivityTermsBinding = ActivityTermsBinding.inflate(layoutInflater)
+
+    override fun initViews() = with(binding)  {
+        ivTermsClose.setOnClickListener { finish() }
+
+        checkboxAllTerms.apply {
+            setOnClickListener {
+                checkboxService.isChecked = isChecked
+                checkboxPersonal.isChecked = isChecked
+                updateBackground(isChecked)
+                updateNextButton()
+            }
+        }
+
+        checkboxService.setOnClickListener {
+            checkboxAllTerms.isChecked = false
+            updateBackground(false)
+            updateNextButton()
+        }
+
+        checkboxPersonal.setOnClickListener {
+            checkboxAllTerms.isChecked = false
+            updateBackground(false)
+            updateNextButton()
+        }
+    }
+
+    private fun updateBackground(isUpdate: Boolean) {
+        when(isUpdate) {
+            true -> binding.layoutTerms.setBackgroundResource(Drawable.bg_brand30_outline_brand60_radius_10dp)
+            false -> binding.layoutTerms.setBackgroundResource(Drawable.bg_white_outline_radius_10dp)
+        }
+    }
+
+    private fun updateNextButton() {
+        val isActive = binding.checkboxService.isChecked && binding.checkboxPersonal.isChecked
+        when (isActive) {
+            true -> updateState(true)
+            false -> updateState(false)
+        }
+    }
+
+    private fun updateState(isActive: Boolean) {
+        binding.tvStart.apply {
+            isEnabled = isActive
+            backgroundTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    this@TermsActivity,
+                    if(isActive) Color.brand100 else Color.brand50
+                )
+            )
+        }
     }
 }
