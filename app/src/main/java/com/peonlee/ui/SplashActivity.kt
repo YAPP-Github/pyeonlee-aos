fa72c9c875d29b5bcc04f05aecef0e7c7ef329a3
jiwon2724 <stopone3119@naver.com>
2023-07-26 02:09:02 +0900

    [REFACTOR] 스플래쉬 코드 개선

--- app/src/main/java/com/peonlee/ui/SplashActivity.kt
+++ app/src/main/java/com/peonlee/ui/SplashActivity.kt
@@ -14,15 +14,18 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.peonlee.R
+import com.peonlee.data.local.LocalDataSource
 import com.peonlee.login.LoginActivity
 import com.peonlee.login.LoginViewModel
 import com.peonlee.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
-    private val loginViewModel: LoginViewModel by viewModels()
+    @Inject lateinit var dataStore: LocalDataSource
     private var isReady = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +44,7 @@ class SplashActivity : AppCompatActivity() {
                 override fun onPreDraw(): Boolean {
                     return if (isReady) {
                         content.viewTreeObserver.removeOnPreDrawListener(this)
-                        observe()
+                        getToken()
                         true
                     } else {
                         false
@@ -51,22 +54,20 @@ class SplashActivity : AppCompatActivity() {
         )
     }
 
-    private fun observe() {
+    private fun getToken() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                loginViewModel.getToken().collect { accessToken ->
-                    if (accessToken.isNotEmpty()) {
-                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                    } else {
-                        startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                    }
-                    finish()
+            dataStore.getAccessToken().collect { accessToken ->
+                if (accessToken.isNotEmpty()) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                } else {
+                    startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
                 }
+                finish()
             }
         }
     }
 
     companion object {
-        const val DELAY = 1500L
+        const val DELAY = 1000L
     }
 }
