7e01e337f6fad0299476156f2889154373504c2f
정지원 <stopone3119@gmail.com>
2023-08-04 23:05:43 +0900

    Refactor login logo (#110)
    * [REFACTOR] 로그인 버튼 ImageView -> TextView 수정
    
    * [REFACTOR] versionCode 수정 5 -> 6
    
    * [REFACTOR] RecyclerView 잘리는 이슈 수정 및 상품 Title maxLines 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     namespace = "com.peonlee"
     defaultConfig {
         applicationId = "com.peonlee"
-        versionCode = 1
+        versionCode = 6
         versionName = "1.0"
     }
 }
