ff7547c28af18e5fa9668f1486bdc2af077f6370
정지원 <stopone3119@gmail.com>
2023-08-05 00:08:35 +0900

    v0.0.1 (#111)
    * [HOTFIX] 홈 화면 편의점별 상품이 없는 경우, UI가 찌그러져 보이는 현상 수정
    
    * [REFACTOR] accessToken -> idToken 수정
    
    * [REFACTOR] 로그인 버튼 ImageView -> TextView 수정 (#106)
    
    * [HOTFIX] 187 - 닉네임 줄넘김 방지 (#108)
    
    * Refactor login logo (#110)
    
    * [REFACTOR] 로그인 버튼 ImageView -> TextView 수정
    
    * [REFACTOR] versionCode 수정 5 -> 6
    
    * [REFACTOR] RecyclerView 잘리는 이슈 수정 및 상품 Title maxLines 설정
    
    ---------
    
    Co-authored-by: aurora32s <aurora32s@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     namespace = "com.peonlee"
     defaultConfig {
         applicationId = "com.peonlee"
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
     }
 }
