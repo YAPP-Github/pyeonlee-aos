12ebe5208a17ccdf2460c53cac61cffd6416f3e1
jiwon2724 <stopone3119@naver.com>
2023-10-10 19:54:14 +0900

    [UI] 보관함 RecyclerView Item 구현

--- feature/storage/build.gradle.kts
+++ feature/storage/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:common"))
     implementation(project(":core:data"))
+    implementation(libs.androidx.cardview)
 }
